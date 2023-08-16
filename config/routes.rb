602650a7a0f74805a5891c3eae83fee03088007c
rafay-qayyum <rafay.qayyum@pakeventures.com>
2023-08-16 11:17:07 +0500

    removed files from being tracked

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root "login#new"
-  resources :articles do
-    resources :comments
-  end
-  resources :login, only: [:new,:create]
-  resources :user, only: [:new,:create]
-  get 'logout', to: 'login#destroy'
-end
